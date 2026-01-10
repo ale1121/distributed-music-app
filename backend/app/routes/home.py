d94ccf69274c3e2e45dfaa470a12debf1d83e906
ale1121 <alexandra.gavrilut@gmail.com>
2026-01-10 18:16:26 +0200

    fixed recent songs ordering

--- backend/app/routes/home.py
+++ backend/app/routes/home.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, session, render_template, current_app
 from app.utils.user_roles import get_user_roles
-from sqlalchemy import select
+from sqlalchemy import select, func
 from app.db import Session
 from app.models import Song, Artist, Album, Play, User
 
@@ -16,18 +16,22 @@ def view():
         return render_template("pages/login.html")
     
     user_id = session["user_id"]
+
+    all_plays = select(Play.song_id,
+                    func.max(Play.timestamp).label("last_played")) \
+                .where(Play.user_id == user_id) \
+                .group_by(Play.song_id) \
+                .subquery()
     
     stmt = select(Song.id, Song.title, 
                   Album.id.label("album_id"), Album.title.label("album_title"),
                   Album.cover_file,
                   User.display_name.label("artist_name")) \
-            .join(Play, Play.song_id == Song.id) \
+            .join(all_plays, all_plays.c.song_id == Song.id) \
             .join(Album, Album.id == Song.album_id) \
             .join(Artist, Artist.id == Album.artist_id) \
             .join(User, User.id == Artist.id) \
-            .where(Play.user_id == user_id) \
-            .distinct(Play.song_id) \
-            .order_by(Play.song_id, Play.timestamp.desc()) \
+            .order_by(all_plays.c.last_played.desc()) \
             .limit(8)
     recents = Session.execute(stmt).all()
 
