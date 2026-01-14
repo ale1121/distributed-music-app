bf9525e44b7879776c4b08ca2fe051aa82ac1b6a
ale1121 <alexandra.gavrilut@gmail.com>
2026-01-14 12:10:31 +0200

    only index public albums

--- backend/app/utils/opensearch/bulk_index.py
+++ backend/app/utils/opensearch/bulk_index.py
@@ -7,13 +7,14 @@ from .conf import INDEX_NAME, OPENSEARCH_URL
 
 
 def bulk_index_catalog():
-    """ Bulk index all existing artists, albums and songs """
+    """ Bulk index all existing artists, public albums and songs """
     artists_stmt = select(Artist.id, User.display_name) \
                     .join(User, User.id == Artist.id)
     artists = Session.execute(artists_stmt).all()
 
     albums_stmt = select(Album.id, Album.title, User.display_name.label("artist_name")) \
-                    .join(User, User.id == Album.artist_id)
+                    .join(User, User.id == Album.artist_id) \
+                    .where(Album.published == True)
     albums = Session.execute(albums_stmt).all()
 
     songs_stmt = select(Song.id, Song.title, Album.id.label("album_id"),
